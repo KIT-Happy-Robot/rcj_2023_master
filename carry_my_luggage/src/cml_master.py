48fc8c2fd29f177b2fc51c576e4df2b9a2540d5e
HaruTaniguchi <c1213803@st.kanazawa-it.ac.jp>
2023-03-23 18:43:56 +0900

    create master

---
+++ carry_my_luggage/src/cml_master.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Carry My Lugagge
+# ルールブック：https://github.com/RoboCupAtHomeJP/Rule2022/blob/master/rules/opl/carry_my_luggage.md
+
+import sys
+import rospy
+import roslib
+import actionlib
+import smach
+import smach_ros
+
+from std_msgs.msg import String, Float64
+from happymimi_navigation.srv import NaviLocation
+from enter_room.srv import EnterRoom
+from happymimi_voice_msgs.srv import TTS, YesNo, ActionPlan
+from actplan_executor.msg import APExecutorAction, APExecutorGoal
+from find_bag.srv import FindBagSrv
+
+
+
+tts_srv = rospy.ServiceProxy('/tts', TTS)
+
+
+class FindBag(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,outcomes=['find_success'])
+        self.sub = rospy.Subscriber("/left_right_recognition", String)
+        self.fb_sub = rospy.Subscriber('/find_bag_server', FindBagSrv)
+
+    def LRCB(self, msg):
+        self.lrmsg = msg
+
+    def execute(self, userdate):
+        if self.lrmsg == 'right':
+
+
+        return 'find_success'
+
+
+class Chaser(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,outcomes=[''])
+
+
+    def execute(self, userdate):
+        
+        return
+
+
+
+class Return(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,outcomes=[''])
+
+
+    def execute(self, userdate):
+        return
+
+
+
+
+if __name__=='__main__':
+    rospy.init_node('cml_master')
+    rospy.loginfo("Start")
+    sm_top = smach.StateMachine('finish_sm_top')
+
+    with sm_top:
+        smach.StateMachine.add(
+            '',
+            (),
+            transitions={})
+
+
+        smach.StateMachine.add(
+            '',
+            (),
+            transitions={})
+
+        smach.StateMachine.add(
+            '',
+            (),
+            transitions={})
+
+    outcome = sm_top.execute()
