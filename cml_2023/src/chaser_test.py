beb111e92e3a3eaeadb083194de55fbaf2663444
KIT Happy Robot <kit.robocup.home@gmail.com>
2023-04-07 11:49:20 +0900

    a

--- cml_2023/src/chaser_test.py
+++ cml_2023/src/chaser_test.py
@@ -45,6 +45,7 @@ class Chaser(smach.State):
         self.start_time = time.time()
         self.find_msg = 'NULL'
         self.cmd_sub = 0.0
+        self.cmd_count = 0
 
     def findCB(self, receive_msg):
         self.find_msg = receive_msg.data
@@ -61,15 +62,17 @@ class Chaser(smach.State):
             #print(self.cmd_sub)
             #print("nt = ",now_time)
             ####
-            if self.cmd_sub == 0.0 and self.find_msg == 'NULL':
-            #if self.cmd_sub == 0.0:
-                self.find_msg = 'lost_stop'
-                self.start_time = time.time()
+            # if self.cmd_sub == 0.0 and self.find_msg == 'NULL':
+            if self.cmd_sub == 0.0:
+                #self.find_msg = 'lost_stop'
+                #self.start_time = time.time()
                 #rospy.loginfo('loststoped')
                 print("0.0 nt = ",now_time)
+                self.cmd_count += 1
                 
             #elif self.cmd_sub == 0.0 and now_time >= 4.0 and self.find_msg == 'lost_stop':
-            elif self.cmd_sub == 0.0 and now_time >= 5.0:
+            #elif self.cmd_sub == 0.0 and now_time >= 5.0:
+            elif now_time >= 5.0:
                 wave_srv("/cml/car_question")
                 rospy.loginfo('yes_or_no')
                 answer = self.yesno().result
@@ -89,6 +92,13 @@ class Chaser(smach.State):
                 print("nt = ",now_time)
                 #self.find_msg = 'NULL'
                 #now_time = 0
+                ###追加
+                self.start_time = time.time()
+                self.cmd_count = 0
+                ###
+                
+            elif self.cmd_count >= 30:
+                return 'chaser_finish'
 
             else: 
                 pass
